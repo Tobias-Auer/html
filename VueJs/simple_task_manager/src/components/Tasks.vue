1f56294d3aa00db413fda6a6f00f6fbe2f2c8698
Tobias Auer <tobias.auer@aol.com>
2024-01-15 20:19:54 +0100

    Started little task tracker with VueJs
    Started little Task Tracker with VueJs
    Worked with basic concepts like components, $emit and events
    
    https://www.youtube.com/watch?v=qZXt1Aom3Cs

---
+++ VueJs/simple_task_manager/src/components/Tasks.vue
@@ -0,0 +1,21 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">
+        <Task @delete-task="$emit('delete-task', task.id)" @double-click="$emit('double-click', task.id)" :task="task" />
+    </div>
+</template>
+
+
+<script>
+import Task from "./Task"
+
+export default {
+    name: "Tasks",
+    components: {
+        Task
+    },
+    props: {
+        tasks: Array
+    },
+    emits: ["delete-task", "double-click"]
+}
+</script>
